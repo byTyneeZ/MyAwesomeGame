585a2e71546cefef47dfbdbaeb250e0414fbb03e
byTyneeZ <36154245+byTyneeZ@users.noreply.github.com>
2018-03-13 12:08:16 +0100

    quit implemented and progress with the images

--- Project/Main.cpp
+++ Project/Main.cpp
@@ -1,6 +1,9 @@
 #include "SDL/Include/SDL.h"
+#include "SDL_Image/Include/SDL_image.h"
 #pragma comment(lib,"SDL/Libraries/SDL2.lib")
 #pragma comment(lib,"SDL/Libraries/SDL2main.lib")
+#pragma comment(lib,"SDL_Image/Lib/SDL2_image.lib")
+
 
 int main(int argc, char* args[]) 
 {
@@ -8,17 +11,50 @@ int main(int argc, char* args[])
 	SDL_Renderer* blockRender = NULL;
 	SDL_Event e;
 	SDL_Rect rect;
-	rect.w = 80;
+	rect.w = 30;
 	rect.h = 70;
-	rect.x = 250;
+	rect.x = 50;
 	rect.y = 250;
+
 	SDL_Rect blast;
 	blast.w = 30;
 	blast.h = 10;
 	blast.x = -150;
 	blast.y = -150;
-	const int SCREEN_WIDTH = 1280, SCREEN_HEIGHT = 680;
+
+	const int SCREEN_WIDTH = 1000, SCREEN_HEIGHT = 563;
 	bool quit = false;
+	bool alive = true;
+
+	struct Bullet{
+		bool shooting;
+	SDL_Rect bullet;
+	};
+
+	struct Enemy {
+		SDL_Rect collision;
+		bool render;
+	};
+
+	struct Boss {
+		SDL_Rect boss;
+		int live;
+	};
+
+	IMG_Init(IMG_INIT_PNG);
+
+	SDL_Surface *image = IMG_Load("background.png");
+	SDL_Texture *image_texture = SDL_CreateTextureFromSurface(blockRender, image);
+	SDL_FreeSurface(image);
+
+	SDL_Rect texture_destination;
+	texture_destination.x = 0;
+	texture_destination.y = 0;
+	texture_destination.w = 1000;
+	texture_destination.h = 563;
+
+	SDL_RenderCopy(blockRender, image_texture, NULL, &texture_destination);
+
 	
 	
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -62,9 +98,12 @@ int main(int argc, char* args[])
 								rect.x -= 10;
 								break;
 							case SDLK_SPACE:
-								blast.x = rect.x + 80;
+								blast.x = rect.x + 30;
 								blast.y = rect.y + 30;
 								blast.x++;
+								break;
+							case SDLK_ESCAPE:
+								quit = true;
 								break;
 							}
 						}
@@ -82,6 +121,10 @@ int main(int argc, char* args[])
 			}
 			
 		}
+
+		SDL_DestroyTexture(image_texture);
+		IMG_Quit();
+		SDL_DestroyRenderer(blockRender);
 		SDL_DestroyWindow(window);
 		SDL_Quit();
 		return 0;
